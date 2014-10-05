59a84ce3049b247e7e359ee8a7d7bc8f0a01be77
flickoid <chris.oatley@gmail.com>
2014-10-05 20:00:05 +0100

    Added new posts and editing posts features

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,25 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.create(params[:post].permit(:description))
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(params[:post].permit(:description))
+    redirect_to posts_path
   end
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -7,7 +7,65 @@ describe 'posts' do
     it 'should display a prompt to add a post' do
       visit '/posts'
       expect(page).to have_content "No posts yet"
-      expect(page).to have_link "Add a post"
+      expect(page).to have_link "Make a post"
+    end
+  end
+
+  context 'posts have been added' do
+    before do
+      Post.create(description: "My first nudibranch")
+    end
+
+    it 'should display them' do
+      visit '/posts'
+      expect(page).to have_content "My first nudibranch"
+      expect(page).not_to have_content "No posts yet"
     end
   end
 end
+
+describe 'creating posts' do
+  it 'prompts the user to fill out a form then displays the new post' do
+    visit '/posts'
+    click_link 'Make a post'
+    fill_in 'Description', with: "My first nudibranch"
+    click_button 'Create post'
+    expect(page).to have_content "My first nudibranch"
+    expect(current_path).to eq '/posts'
+  end
+end
+
+describe 'editing posts' do
+
+  before do
+    Post.create(description: "My first nudibranch")
+  end
+
+   it 'can allow a user to edit a post' do
+    visit '/posts'
+    click_link 'Edit post'
+    fill_in 'Description', with: "Still the same nudibranch"
+    click_button 'Update post'
+    expect(page).to have_content "Still the same nudibranch"
+    expect(current_path).to eq '/posts'
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
